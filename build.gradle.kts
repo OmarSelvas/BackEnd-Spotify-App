c12bb7cec2f391d809b93cf5eb749cb1ee745f1b
0marS3lvas <omarselvas153@gmail.com>
2025-12-02 21:27:47 -0600

    reestructuracion de la API

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+val kotlin_version: String by project
+val logback_version: String by project
+val ktor_version: String by project
+
 plugins {
     kotlin("jvm") version "2.2.20"
     kotlin("plugin.serialization") version "2.2.20"
@@ -8,23 +12,24 @@ group = "com.example"
 version = "0.0.1"
 
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+    mainClass = "io.ktor.server.netty.EngineMain"
 }
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
 }
 
 dependencies {
     // Ktor Server
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-netty")
+
     implementation("io.ktor:ktor-server-auth")
     implementation("io.ktor:ktor-server-content-negotiation")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
     implementation("io.ktor:ktor-server-status-pages")
     implementation("io.ktor:ktor-server-config-yaml")
-
 
     // Database
     implementation("org.postgresql:postgresql:42.7.1")
@@ -34,9 +39,9 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:0.46.0")
     implementation("org.jetbrains.exposed:exposed-java-time:0.46.0")
 
-    // Koin for DI
-    implementation("io.insert-koin:koin-ktor:3.5.3")
-    implementation("io.insert-koin:koin-logger-slf4j:3.5.3")
+    // Koin for DI (ACTUALIZADO A 4.0.0 PARA KTOR 3)
+    implementation("io.insert-koin:koin-ktor:4.0.0")
+    implementation("io.insert-koin:koin-logger-slf4j:4.0.0")
 
     // Logging
     implementation("ch.qos.logback:logback-classic:1.4.14")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,18 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        // Necesario para la resolución de plugins
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+    }
+}
+
 rootProject.name = "ApiSpotify"
 
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
+        // Necesario para la resolución de dependencias
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
     }
 }
